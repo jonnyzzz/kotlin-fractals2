edebe4bc01418ae4023b01bddb3bc1854b02c56d
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-06-22 17:08:37 +0300

    switch to Kotlin MPP project, reimplement run task
    Signed-off-by: Eugene Petrenko <eugene.petrenko@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   kotlin("multiplatform") version "1.3.31"
-  application
 }
 
 repositories {
@@ -10,20 +9,26 @@ repositories {
 
 kotlin {
   jvm()
-}
 
-val ktorVersion = "1.2.1"
-val logbackVersion = "1.2.3"
+  val ktorVersion = "1.2.1"
+  val logbackVersion = "1.2.3"
 
-kotlin.sourceSets["jvmMain"].dependencies {
-  implementation(kotlin("stdlib-jdk8"))
-  implementation("io.ktor:ktor-server-netty:$ktorVersion")
-  implementation("io.ktor:ktor-html-builder:$ktorVersion")
-  implementation("ch.qos.logback:logback-classic:$logbackVersion")
-}
+  sourceSets["jvmMain"].dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-html-builder:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+  }
 
-application.mainClassName = "org.jonnyzzz.kotlin.fractals2.MainKt"
-
-tasks.run.configure {
-  systemProperty("java.awt.headless", "true")
+  val run by tasks.creating(JavaExec::class) {
+    group = "application"
+    dependsOn(targets["jvm"].compilations["main"].compileAllTaskName)
+    main = "org.jonnyzzz.kotlin.fractals2.MainKt"
+    classpath(
+      { targets["jvm"].compilations["main"].output.allOutputs.files },
+      { configurations["jvmRuntimeClasspath"] }
+    )
+    ///disable app icon on macOS
+    systemProperty("java.awt.headless", "true")
+  }
 }
