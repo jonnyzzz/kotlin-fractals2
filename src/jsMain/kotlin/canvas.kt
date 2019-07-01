15f47a2e8a33019d6848215ed2d9b835cd911c3b
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-07-02 00:15:31 +0300

    simplify code
    Signed-off-by: Eugene Petrenko <eugene.petrenko@gmail.com>

--- src/jsMain/kotlin/canvas.kt
+++ src/jsMain/kotlin/canvas.kt
@@ -3,45 +3,29 @@ package org.jonnyzzz.kotlin.fractals2
 import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
 
+fun renderToCanvas(canvas : HTMLCanvasElement, action: (FractalImage) -> Unit) {
+  val ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+  ctx.clearRect(0.0, 0.0, ctx.canvas.width.toDouble(), ctx.canvas.height.toDouble())
 
-inline fun renderToCanvas(canvas : HTMLCanvasElement, action: (FractalImage) -> Unit) {
-  val image = JSFractalImage(canvas.getContext("2d") as CanvasRenderingContext2D)
-  try {
-    action(image)
-  } finally {
-    image.commit()
-  }
-}
-
-class JSFractalImage(
-  private val ctx: CanvasRenderingContext2D
-) : FractalImage {
-  init {
-    ctx.clearRect(0.0, 0.0, ctx.canvas.width.toDouble(), ctx.canvas.height.toDouble())
-  }
-
-  private val imageData by lazy {
-    ctx.createImageData(
+  val imageData = ctx.createImageData(
       ctx.canvas.width.toDouble(),
       ctx.canvas.height.toDouble())
+
+  val image = object:FractalImage {
+    override val pixelRect
+      get() = Rect(
+        left = 0, top = 0,
+        right = imageData.width, bottom = imageData.height)
+
+    override fun putPixel(p: Pixel, c: Color) {
+      val base = 4 * (p.x + imageData.width * p.y)
+      val image : dynamic = imageData.data
+      image[base + 0] = c.r
+      image[base + 1] = c.g
+      image[base + 2] = c.b
+      image[base + 3] = 255
+    }
   }
-
-  override val pixelRect
-    get() = Rect(left = 0, top = 0, right = imageData.width, bottom = imageData.height)
-
-  override fun putPixel(p: Pixel, c: Color) {
-    val base = 4 * (p.x + imageData.width * p.y)
-
-    val image : dynamic = imageData.data
-
-    image[base + 0] = c.r
-    image[base + 1] = c.g
-    image[base + 2] = c.b
-    image[base + 3] = 255
-  }
-
-  fun commit() {
-    println("Commit image to ctx: width=${imageData.width}, height=${imageData.height}")
-    ctx.putImageData(imageData, 0.0, 0.0)
-  }
+  action(image)
+  ctx.putImageData(imageData, 0.0, 0.0)
 }
